edf718150a8fade8fda8c0c1a405b0c6f16a4bfd
Todd Schwartzfarb <todd@wirestreak.com>
2016-08-25 22:59:13 -0400

    Vowel Count Kata

---
+++ vowel_count.rb
@@ -0,0 +1,17 @@
+VOWELS = ["a","e","i","o","u"]
+
+def getCount(inputStr)
+  inputStr.chars.select { | char | VOWELS.include? char }.size
+end
+
+puts getCount("abracadabra")
+puts getCount ("a".."z").to_a.join
+
+__END__
+
+7 kyu
+Vowel Count
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
