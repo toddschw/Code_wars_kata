3fd0dd0944c3a06d703bc7116687d138afd1b84d
Todd Schwartzfarb <todd@wirestreak.com>
2016-08-25 22:15:04 -0400

    Convert an array of strings to numbers

---
+++ convert_an_array_of_strings_to_numbers.rb
@@ -0,0 +1,29 @@
+def to_number_array(string_array)
+  string_array.map do |value|
+    if value.index(".")
+      value.to_f
+    else
+      value.to_i
+    end
+  end
+end
+
+__END__
+
+7 kyu
+Convert an array of strings to array of numbers
+
+Oh no!
+
+Some really funny web dev gave you an array of numbers from his API response as an array of strings!
+
+You need to cast the whole array to the correct type.
+
+Create the function
+
+Ruby: toNumberArray(array)
+that takes as a parameter an array of numbers represented as strings and outputs an array of numbers.
+
+ie:["1", "2", "3"] to [1, 2, 3]
+
+Note that you can receive floats as well.
